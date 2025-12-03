cbff9726aff5f3ce1a98058c29471564aeaec91e
Venu Siddapura Govindaraju <govindaraju.venu@outlook.com>
2025-12-03 17:52:16 +0100

    written greter then 3 number program

---
+++ 01_python_basics/List_if_conditions_Loops/If_condition/n>3number.py
@@ -0,0 +1,16 @@
+print("welcome to out play a greater then 3 number program ")
+
+num1 = int(input("Enter a first number:  "))
+num2 = int(input("Enter a second number:  "))
+num3 = int(input("Enter a third number:  "))
+
+print("The numbers you entered are:", num1, num2, num3)
+print(type(num1), type(num2), type(num3))
+
+if num1 > num2 and num1 > num3 :
+    print(f"{num1} is the grestes number")
+elif num2 > num1 and num2 > num3 :
+    print(f"{num2} is the grestes number")
+else :
+    print(f"{num3} is the grestes number")
+    
